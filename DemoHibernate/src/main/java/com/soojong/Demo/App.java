51279f7e07c4fca70a2672d4939288508126d2f2
soojong <hsz3855@naver.com>
2021-09-07 08:47:58 +0900

    fethcing data

--- DemoHibernate/src/main/java/com/soojong/Demo/App.java
+++ DemoHibernate/src/main/java/com/soojong/Demo/App.java
@@ -13,24 +13,34 @@ public class App {
 
     public static void main(String[] args) {
 
-        Student student = new Student();
-        student.setStudent_id(1);
-        student.setName("Soojong");
-        student.setAddr("Seoul");
-        student.setGrade(3);
+        // insert할 데이터를 셋팅한다.
+        //Student student = new Student();
 
 
+//        student.setStudent_id(1);
+//        student.setName("Soojong");
+//        student.setAddr("Seoul");
+//        student.setGrade(3);
+
+        // configure()메소드에 아무값도 입력하지 않으면 hibernate.cfg.xml을 default로 읽어온다
         Configuration con = new Configuration().configure().addAnnotatedClass(Student.class);
+
 
         ServiceRegistry service = new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
 
+        // SessionFactory는 Database와 연결을 관리한다. DB 1개당 SessionFactory 1개가 필요하다
         SessionFactory sf = con.buildSessionFactory(service);
 
         Session session = sf.openSession();
 
         Transaction tx = session.beginTransaction();
 
-        session.save(student);
+        // student 테이블의 student_id가 1인 데이터 가져오기
+        Student s = session.get(Student.class, 1);
+        System.out.println(s);
+
+        //insert를 수행한다.
+        //session.save(student);
 
         tx.commit();
 
--- DemoHibernate/src/main/java/com/soojong/Demo/Student.java
+++ DemoHibernate/src/main/java/com/soojong/Demo/Student.java
@@ -1,13 +1,16 @@
 package com.soojong.Demo;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+//@Table(name = "table_student") // 테이블 이름 입력, 없으면 Entity 이름, 없으면 클래스이름이 테이블 명이 된다.
 public class Student { // POJO
 
     @Id
     private int student_id;
+
+    @Column(name= "student_name") // DB에 반영될 컬럼명 지정
     private String name;
+    @Transient // DB에 반영되지 않음
     private String addr;
     private int grade;
 
@@ -44,4 +47,13 @@ public class Student { // POJO
         this.grade = grade;
     }
 
+    @Override
+    public String toString() {
+        return "Student{" +
+                "student_id=" + student_id +
+                ", name='" + name + '\'' +
+                ", addr='" + addr + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
 }
